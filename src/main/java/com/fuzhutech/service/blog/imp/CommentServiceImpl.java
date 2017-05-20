dca6d568f4da04ec2e8b42e69b5289ee3385bce6
fuzhutech <fuzhutech@163.com>
2017-05-21 00:16:40 +0800

    新增评论时设置创建时间

--- src/main/java/com/fuzhutech/service/blog/imp/CommentServiceImpl.java
+++ src/main/java/com/fuzhutech/service/blog/imp/CommentServiceImpl.java
@@ -7,8 +7,18 @@ import com.fuzhutech.pojo.blog.Comment;
 import com.fuzhutech.service.blog.CommentService;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+
 @Service
 public class CommentServiceImpl extends BaseServiceImpl<Comment> implements CommentService {
+
+    @Override
+    public Integer add(Comment record) {
+        //设置创建时间
+        Date date = new Date();
+        record.setCreateTime(date);
+        return this.mapper.insert(record);
+    }
 
     @Override
     public int selectAllCount() {
